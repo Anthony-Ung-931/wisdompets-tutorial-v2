99ca4ba4421a98527502d52acf080ae48f4bdd49
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-05-29 10:49:19 -0400

    Created dummy templates for home and pet_detail

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 def home(request):
-    return HttpResponse('<p>home view</p>')
+    pets = Pet.objects.all()
+    return render(request, template_name='home.html', 
+                    context={'pets': pets,})
 
 def pet_detail(request, pet_id):
-    return HttpResponse(f'<p>pet_detail view with id {pet_id}</p>')
+    try:
+        pet = Pet.objects.id(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('pet not found')
+    return render(request, template_name='pet.detail.html', 
+                    context={'pet': pet,})
