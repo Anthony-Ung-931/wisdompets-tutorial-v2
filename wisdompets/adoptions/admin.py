082cfea20fc1708c78c2ff0a0a334b19620c4085
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-05-29 10:15:58 -0400

    Modified admin.py

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Pet
 # Register your models here.
+
+@admin.register(Pet) # Decorator
+class PetAdmin(admin.ModelAdmin):
+    pass # Makes this a valid class.
